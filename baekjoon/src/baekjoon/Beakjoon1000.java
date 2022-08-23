41bae8d4c918d595eed6314c77efc2413b90972a
jongchi <84554083+jongchi@users.noreply.github.com>
2022-08-24 00:09:37 +0900

    백준 1000

---
+++ baekjoon/src/baekjoon/Beakjoon1000.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Beakjoon1000 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String[] input = scan.nextLine().split(" ");	// 입력값을 " "로 나누어서 저장
+		
+		int x = Integer.parseInt(input[0]);
+		int y = Integer.parseInt(input[1]);
+		
+		System.out.println(x+y);
+		scan.close();
+	}
+}
