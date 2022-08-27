48cb85e9712ff0ab03d1bccb27954235bcb135ef
jongchi <84554083+jongchi@users.noreply.github.com>
2022-08-27 11:52:19 +0900

    백준 1001, 1008

--- baekjoon/src/baekjoon/Baekjoon1001.java
+++ baekjoon/src/baekjoon/Baekjoon1001.java
@@ -2,17 +2,19 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class Beakjoon1000 {
-	
+public class Baekjoon1001 {
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);	
 		
 		String[] input = scan.nextLine().split(" ");	// 입력값을 " "로 나누어서 저장
 		
 		int x = Integer.parseInt(input[0]);
 		int y = Integer.parseInt(input[1]);
 		
-		System.out.println(x+y);
+		System.out.println(x-y);					// 1000번 문제에서 +만 -로 변경
 		scan.close();
+
 	}
+
 }
---
+++ baekjoon/src/baekjoon/Baekjoon1008.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long x = sc.nextInt();
+		double y = sc.nextFloat();
+		
+		System.out.println(x/y);
+		sc.close();
+
+	}
+
+}
