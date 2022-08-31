7459903f150bd3b790f84ab238bf2f517b9a72da
jongchi <84554083+jongchi@users.noreply.github.com>
2022-09-01 00:23:34 +0900

    백준 2741, 2742

---
+++ baekjoon/src/baekjoon/Baekjoon2741.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		for(int i = 1; i<=input; i++) {
+			System.out.println(i);
+		}
+		sc.close();
+
+	}
+
+}
