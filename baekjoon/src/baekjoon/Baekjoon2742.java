0e162c425d899c845547c06db3ec1d2010409ea9
jongchi <84554083+jongchi@users.noreply.github.com>
2022-09-01 00:24:03 +0900

    백준 2742

---
+++ baekjoon/src/baekjoon/Baekjoon2742.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2742 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int input = Integer.parseInt(bf.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = input; i>0; i--) {
+			sb.append(i+"\n");
+		}
+
+		System.out.println(sb.toString());
+		bf.close();
+	}
+
+}
