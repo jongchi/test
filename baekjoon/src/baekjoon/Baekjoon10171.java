63655ae5ba76d3b62797bfb3ccad5ebe255c20d4
jongchi <84554083+jongchi@users.noreply.github.com>
2022-09-09 21:33:45 +0900

    백준 10171

---
+++ baekjoon/src/baekjoon/Baekjoon10171.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+public class Baekjoon10171 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\\n")
+			.append(" )  ( ')\n")
+			.append("(  /  )\n")
+			.append(" \\(__)|");
+		
+		System.out.println(sb);
+		
+	}
+
+}
