e06511469b3814b5a5372e7a27aeb02e3f7b2883
jongchi <84554083+jongchi@users.noreply.github.com>
2022-09-10 19:44:34 +0900

    백준 10871

---
+++ baekjoon/src/baekjoon/Baekjoon10871.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Baekjoon10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			int input = sc.nextInt();
+			if(input < X) {
+				sb.append(input+" ");
+			}
+		}
+		System.out.println(sb);
+		
+	}
+
+}
