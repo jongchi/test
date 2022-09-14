219b29924e01cbed9625c82af4ab9eb779b31554
jongchi <sesi8624@gmail.com>
2022-09-14 18:39:19 +0900

    백준 10816

---
+++ baekjoon/src/baekjoon/Baekjoon10816.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Baekjoon10816 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		HashMap<Integer, Integer> card = new HashMap<Integer, Integer>();
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.valueOf(bf.readLine());					
+		StringTokenizer stk = new StringTokenizer(bf.readLine()," ");				
+		
+		StringBuilder sb = new StringBuilder();
+		while(stk.hasMoreTokens()) {
+			int num = Integer.valueOf(stk.nextToken());
+			if(card.get(num) != null) {
+				card.put(num, card.get(num)+1);
+			} else {
+				card.put(num, 1);
+			}
+		}
+		
+		int M = Integer.valueOf(bf.readLine());
+		stk = new StringTokenizer(bf.readLine()," ");
+		while(stk.hasMoreTokens()) {
+			int num = Integer.valueOf(stk.nextToken());
+			if(card.get(num) != null) {
+				sb.append(card.get(num)+" ");
+			} else {
+				sb.append(0+" ");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
