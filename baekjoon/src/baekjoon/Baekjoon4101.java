faa9e201f45901ebd339198668cb5f9961475e8b
jongchi <84554083+jongchi@users.noreply.github.com>
2022-09-24 07:31:36 +0900

    백준 4101

---
+++ baekjoon/src/baekjoon/Baekjoon4101.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon4101 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		while(true) {
+			stk = new StringTokenizer(bf.readLine());
+			int x = Integer.valueOf(stk.nextToken());
+			int y = Integer.valueOf(stk.nextToken());
+			
+			if(x == 0 && y == 0) {
+				return;
+			}
+			
+			System.out.println(x > y ? "Yes" : "No");
+			
+		}
+
+	}
+
+}
