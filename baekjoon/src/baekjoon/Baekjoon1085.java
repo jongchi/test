aa6eeafde93d961f49bec9c2b8e974b8c517f215
jongchi <sesi8624@gmail.com>
2022-09-27 19:52:58 +0900

    백준 1085

---
+++ baekjoon/src/baekjoon/Baekjoon1085.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1085 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(bf.readLine());
+		
+		int result = 0;
+		
+		int x = Integer.valueOf(stk.nextToken());
+		int y = Integer.valueOf(stk.nextToken());
+		int w = Integer.valueOf(stk.nextToken());
+		int h = Integer.valueOf(stk.nextToken());
+		
+		int n = w - x < x ? w - x : x;							// 가로 길이 계산
+		int m = h - y < y ? h - y : y;							// 세로 길이 계산
+				
+		result = n < m ? n : m; 								// 작은 숫자를 값으로
+		
+		System.out.println(result);
+	}
+
+}
