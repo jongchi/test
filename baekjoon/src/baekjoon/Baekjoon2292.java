a6daf0f8af2d26f50fa329dccff7973ac13d87da
jongchi <sesi8624@gmail.com>
2022-09-30 18:16:58 +0900

    백준 2292

---
+++ baekjoon/src/baekjoon/Baekjoon2292.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon2292 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.valueOf(bf.readLine()) - 1;			// 계산 용이하게 하기 위해 1 빼주고 시작
+		int count = 1;
+		
+		while(n > 0) {				
+			n -= 6*count;									// 칸 당 6의 배수만큼 늘어남.
+			count++;			
+		}
+				
+		System.out.println(count);
+	}
+
+}
