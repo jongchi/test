2b7f987e200b1177d7b5a390bfc1338788fbd946
jongchi <84554083+jongchi@users.noreply.github.com>
2022-10-08 10:22:59 +0900

    백준 2588

---
+++ baekjoon/src/baekjoon/Baekjoon2588.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2588 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.valueOf(br.readLine());
+		int y = Integer.valueOf(br.readLine());
+		
+		System.out.println(x * (y%10));
+		System.out.println(x * (y/10%10));
+		System.out.println(x * (y/100));
+		System.out.println(x * y);
+	}
+
+}
