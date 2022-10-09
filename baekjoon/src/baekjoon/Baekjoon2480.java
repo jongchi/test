53e7e93a9fd2ac1005729b650b26927117f2fbb5
jongchi <84554083+jongchi@users.noreply.github.com>
2022-10-09 10:39:00 +0900

    백준 2480

---
+++ baekjoon/src/baekjoon/Baekjoon2480.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon2480 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+		
+		int i = Integer.valueOf(stk.nextToken());
+		int j = Integer.valueOf(stk.nextToken());
+		int k = Integer.valueOf(stk.nextToken());
+		int max = 0;
+		if(i==j&&j==k) {
+			System.out.println(10000+i*1000);
+		} else if(i==j&&i!=k) {
+			System.out.println(1000+i*100);
+		} else if(j==k&&i!=j) {
+			 System.out.println(1000+j*100);
+		} else if(i==k&&i!=j) {
+			System.out.println(1000+i*100);
+		} else if(i!=k&&i!=k) {
+			if(i > max) {
+				max = i;
+			}
+			if(j > max) {
+				max = j;
+			}
+			if(k > max) {
+				max = k;
+			}
+			System.out.println(max*100);
+		}
+		
+	}
+
+}
