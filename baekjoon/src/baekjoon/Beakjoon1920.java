3d7c2f3fce2290b267b1a9da012fa973ad39a081
jongchi <84554083+jongchi@users.noreply.github.com>
2022-10-15 11:33:39 +0900

    백준 1920

---
+++ baekjoon/src/baekjoon/Beakjoon1920.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Beakjoon1920 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.valueOf(br.readLine());
+		stk = new StringTokenizer(br.readLine()," ");
+		while(stk.hasMoreTokens()) {
+			map.put(stk.nextToken(),1);
+			n--;
+		}
+		
+		int m = Integer.valueOf(br.readLine());
+		stk = new StringTokenizer(br.readLine()," ");
+		while(stk.hasMoreTokens()) {
+			sb.append(map.getOrDefault(stk.nextToken(), 0)+"\n");			//숫자가 있으면 1 없으면 0
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
