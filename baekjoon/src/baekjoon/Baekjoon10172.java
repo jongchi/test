8c7fa30f92f1737ee69752276d013a81d16f28b9
jongchi <84554083+jongchi@users.noreply.github.com>
2022-10-29 09:05:11 +0900

    백준 10172

---
+++ baekjoon/src/baekjoon/Baekjoon10172.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+public class Baekjoon10172 {
+
+	public static void main(String[] args) {
+		String str = "|\\_/|\n"
+				+ "|q p|   /}\n"
+				+ "( 0 )\"\"\"\\\n"
+				+ "|\"^\"`    |\n"
+				+ "||_/=\\\\__|";
+		
+		System.out.println(str);
+	}
+
+}
