1b34d77d7a036822cc79adb936c7d40e687217a6
jongchi <84554083+jongchi@users.noreply.github.com>
2023-08-10 23:49:16 +0900

    Create Baekjoon10039.java

---
+++ baekjoon/src/baekjoon/Baekjoon10039.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon10039 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		
+		for(int i=0;i<5;i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x < 40) {
+				x = 40;
+			}
+			
+			sum += x;
+		}
+		
+		System.out.println(sum/5);
+
+	}
+
+}
